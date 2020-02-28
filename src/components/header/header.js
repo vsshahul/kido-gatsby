96957a1038485ea8f124905435bfac951be8da39
shahul hameed vs <shahul@f22labs.com>
2020-02-28 12:14:24 +0530

    final

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,9 @@ import logo from '../assets/logo.svg'
 
 const Header = () => {
   return <header className='header'>
-    <a className='logo' href='./'><img src={logo} alt='logo' /></a>
+    <div className='logoCont'>
+      <a className='logo' href='./'><img src={logo} alt='logo' /></a>
+    </div>
     <a className='links' href='./'>About</a>
     <a className='links' href='./'>Contact Us</a>
     <a className='links active' href='./'>Login/Signup</a>
